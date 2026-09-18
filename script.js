6bd56d4d79d4735e74bef52ac14253cbb6ca35c7
sameen shahid <sameenshahid99@gmail.com>
2026-09-18 23:28:29 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector(".nav");
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-menuBtn.addEventListener("click", () => nav.classList.toggle("open"));
-navLinks.forEach(link => link.addEventListener("click", () => nav.classList.remove("open")));
